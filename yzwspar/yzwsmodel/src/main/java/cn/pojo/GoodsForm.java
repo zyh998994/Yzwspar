30b49ef8b541ef8f199aa5a4d52f7da09fffe2cd
zhengyihao <zhengyh998994@163.com>
2018-11-01 15:23:55 +0800

    zyh

--- yzwspar/yzwsmodel/src/main/java/cn/pojo/GoodsForm.java
+++ yzwspar/yzwsmodel/src/main/java/cn/pojo/GoodsForm.java
@@ -16,7 +16,69 @@ public class GoodsForm {
     private Integer sid;    //商家ID
     private Integer gstate; //商品状态
     private Integer tid;    //种类ID
+    private String specs1;
+    private Integer specs2;
+    private String specs3;
+    private String specs4;
+    private String ext1;
+    private String ext2;
+    private String exts3;
 
+    public String getSpecs1() {
+        return specs1;
+    }
+
+    public void setSpecs1(String specs1) {
+        this.specs1 = specs1;
+    }
+
+    public Integer getSpecs2() {
+        return specs2;
+    }
+
+    public void setSpecs2(Integer specs2) {
+        this.specs2 = specs2;
+    }
+
+    public String getSpecs3() {
+        return specs3;
+    }
+
+    public void setSpecs3(String specs3) {
+        this.specs3 = specs3;
+    }
+
+    public String getSpecs4() {
+        return specs4;
+    }
+
+    public void setSpecs4(String specs4) {
+        this.specs4 = specs4;
+    }
+
+    public String getExt1() {
+        return ext1;
+    }
+
+    public void setExt1(String ext1) {
+        this.ext1 = ext1;
+    }
+
+    public String getExt2() {
+        return ext2;
+    }
+
+    public void setExt2(String ext2) {
+        this.ext2 = ext2;
+    }
+
+    public String getExts3() {
+        return exts3;
+    }
+
+    public void setExts3(String exts3) {
+        this.exts3 = exts3;
+    }
 
     public Integer getGid() {
         return gid;
