768a38d5e769fc9244cf5e09b636513ff78a40a2
zhengyihao <zhengyh998994@163.com>
2018-11-01 12:10:52 +0800

    zyh

---
+++ yzwspar/yzwsdao/src/main/java/cn/dao/GoodsNameDao.java
@@ -0,0 +1,5 @@
+package cn.dao;
+
+public interface GoodsNameDao {
+
+}
---
+++ yzwspar/yzwsmodel/src/main/java/cn/pojo/Shopcart.java
@@ -0,0 +1,40 @@
+package cn.pojo;
+
+public class Shopcart {
+    private Integer shopid;
+    private Integer uuid;
+    private Integer goodid;
+    private Integer shopnum;
+
+    public Integer getShopid() {
+        return shopid;
+    }
+
+    public void setShopid(Integer shopid) {
+        this.shopid = shopid;
+    }
+
+    public Integer getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Integer uuid) {
+        this.uuid = uuid;
+    }
+
+    public Integer getGoodid() {
+        return goodid;
+    }
+
+    public void setGoodid(Integer goodid) {
+        this.goodid = goodid;
+    }
+
+    public Integer getShopnum() {
+        return shopnum;
+    }
+
+    public void setShopnum(Integer shopnum) {
+        this.shopnum = shopnum;
+    }
+}
