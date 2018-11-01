8c65d24f7615b412e167c13117371c2a10a6ae8b
lsy18642146883 <44563804+lsy18642146883@users.noreply.github.com>
2018-11-01 12:19:47 +0800

    刘思宇

--- yzwspar/yzwsmodel/src/main/java/cn/pojo/UserForm.java
+++ yzwspar/yzwsmodel/src/main/java/cn/pojo/UserForm.java
@@ -1,22 +1,81 @@
 package cn.pojo;
 
+import java.util.Date;
+
+/**
+ * 用户信息表
+ */
 public class UserForm {
         private Integer uid; //id
-        private String openid; //微信登录的唯一标识
+    private String openid; //微信登录的唯一标识
+    private String country; //用户所在国家
+    private Date greatedate;    //用户创建时间
+    private Integer sex;    //用户性别
+    private String headimg; //用户头像
+    private String nickname;    //用户昵称
+    private String mobileno;    //用户手机号
 
-        public Integer getUid() {
-            return uid;
-        }
+    public String getCountry() {
+        return country;
+    }
 
-        public void setUid(Integer uid) {
-            this.uid = uid;
-        }
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
-        public String getOpenid() {
-            return openid;
-        }
+    public Date getGreatedate() {
+        return greatedate;
+    }
 
-        public void setOpenid(String openid) {
-            this.openid = openid;
-        }
+    public void setGreatedate(Date greatedate) {
+        this.greatedate = greatedate;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getHeadimg() {
+        return headimg;
+    }
+
+    public void setHeadimg(String headimg) {
+        this.headimg = headimg;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
 }
