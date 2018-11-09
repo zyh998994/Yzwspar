827e8487d7aff68913425e6227df8cd4c0e7b8d3
zhengyihao <zhengyh998994@163.com>
2018-11-09 09:44:58 +0800

    zyh

--- yzwspar/yzwsweb/src/main/java/cn/controller/ShopcartController.java
+++ yzwspar/yzwsweb/src/main/java/cn/controller/ShopcartController.java
@@ -3,10 +3,16 @@ package cn.controller;
 import cn.pojo.GoodsForm;
 import cn.pojo.Shopcart;
 import cn.service.ShopcartService;
+import com.wordnik.swagger.annotations.ApiImplicitParam;
+import com.wordnik.swagger.annotations.ApiImplicitParams;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.UnsupportedEncodingException;
 
 @RequestMapping("test/")
 @Controller
@@ -24,25 +30,48 @@ public class ShopcartController {
 
     @ResponseBody
     @RequestMapping(value = "getinfo")
-    public Object getinfo(GoodsForm gf){
-        GoodsForm good = new GoodsForm();
+    @ApiOperation(value = "新增商品到购物车/或者将所添加到购物车的商品数量叠加到购物车里", httpMethod = "POST",
+            notes = "新增/修改数量",response = String.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query" , required = false, name = "specs1", dataType = "String",
+                    value = "规格一"),
+            @ApiImplicitParam(paramType = "query" , required = false, name = "specs2", dataType = "int",
+                    value = "规格一"),
+            @ApiImplicitParam(paramType = "query" , required = false, name = "num", dataType = "int",
+                    value = "数量"),
+            @ApiImplicitParam(paramType = "query" , required = false, name = "uuid", dataType = "int",
+                    value = "用户id"),
+    })
+    public Object getinfo(@RequestParam("specs1") String specs1,@RequestParam("specs2") int specs2,
+                          @RequestParam("num") int num,@RequestParam("uuid") int uuid) throws UnsupportedEncodingException {
+
+        specs1 = new String(specs1.getBytes("iso-8859-1"),"utf-8");
+        GoodsForm gf = new GoodsForm();
         Shopcart sc = new Shopcart();
-        good.setSpecs1("复活节");//型号1
-        good.setSpecs2(9);//型号2
-        int num = 2;//数量
-        sc.setUuid(8);
-        shopcart.seleinfo(good);
-        return shopcart.judge(sc,good,num);
+        gf.setSpecs1(specs1);//型号1
+        gf.setSpecs2(specs2);//型号2
+        sc.setUuid(uuid);
+        shopcart.seleinfo(gf);
+        return shopcart.judge(sc,gf,num);
     }
-
-    @RequestMapping(value = "updanum")
+    /**
+     * 修改数量
+     * @return
+     */
     @ResponseBody
-    public String updashopnum(Shopcart sc, String choose){
-        sc.setUuid(8);
-        sc.setGoodid(13);
-        choose = "y";
-
+    @RequestMapping(value = "updanum")
+    @ApiOperation(value = "修改购物车内商品的数量",httpMethod = "PUT", notes = "修改数量", response = String.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query",required = false,name="uuid",dataType = "int",value = "用户id"),
+            @ApiImplicitParam(paramType = "query",required = false,name="goodid",dataType = "int",value = "商品id"),
+            @ApiImplicitParam(paramType = "query",required = false,name="goodid",dataType = "String",value = "y(增加一个)/n（减去一个）")
+    })
+    public String updashopnum(@RequestParam("uuid") int uuid, @RequestParam("goodid") int goodid, @RequestParam("choose") String choose){
+        Shopcart sc = new Shopcart();
+        sc.setUuid(uuid);
+        sc.setGoodid(goodid);
         return shopcart.updaShopnum(sc,choose);
     }
+
 
 }
--- yzwspar/yzwsweb/src/main/java/cn/controller/ShowshopcartController.java
+++ yzwspar/yzwsweb/src/main/java/cn/controller/ShowshopcartController.java
@@ -2,9 +2,13 @@ package cn.controller;
 
 import cn.pojo.Shopcart;
 import cn.service.ShowShopCartService;
+import com.wordnik.swagger.annotations.ApiImplicitParam;
+import com.wordnik.swagger.annotations.ApiImplicitParams;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -22,17 +26,39 @@ public class ShowshopcartController {
         this.showscs = showscs;
     }
 
+    /**
+     * 显示购物车内接口
+     * @param uuid 用户id
+     * @return
+     */
+
     @RequestMapping(value = "getshowinfo")
     @ResponseBody
-    public List<Shopcart> shoplist(Shopcart sc){
-        sc.setUuid(8);
+    @ApiOperation(value = "显示购物车内商品",httpMethod = "GET", notes = "查询", response = String.class)
+    @ApiImplicitParam(paramType = "query",required = false,name="uuid",dataType = "int",value = "用户id")
+    public List<Shopcart> shoplist(@RequestParam("uuid") int uuid){
+        Shopcart sc= new Shopcart();
+        sc.setUuid(uuid);
         return showscs.shoplist(sc);
     }
+
+    /**
+     * 按照id删除购物车商品
+     * @param uuid 用户ID
+     * @param goodid 商品id
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "deleshop")
-    public String delejudge(Shopcart sc){
-        sc.setUuid(1);
-        sc.setGoodid(-1);
+    @ApiOperation(value = "按照id删除购物车商品",httpMethod = "DELETE", notes = "删除", response = String.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query",required = false,name="uuid",dataType = "int",value = "用户id"),
+            @ApiImplicitParam(paramType = "query",required = false,name="goodid",dataType = "int",value = "商品id")
+    })
+    public String delejudge(@RequestParam ("uuid") int uuid, @RequestParam("goodid") int goodid){
+        Shopcart sc= new Shopcart();
+        sc.setUuid(uuid);
+        sc.setGoodid(goodid);
         return showscs.deleshopinfo(sc);
     }
 }
